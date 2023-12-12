03e6dec8b4de80bdf6c0bf960f93582dcc525865
Yusuf <feezynharmony@gmail.com>
2023-12-12 19:54:04 +0100

    0-javascript_is_amazing.js

--- 0x12-javascript-warm_up/0-javascript_is_amazing.js
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -1,8 +1,3 @@
-#!/usr/bin/env node
-// Filename: 0-javascript_is_amazing.js
-
-// Create a constant variable called myVar with the value “JavaScript is amazing”
+#!/usr/bin/node
 const myVar = 'JavaScript is amazing';
-
-// Use console.log(...) to print the output
 console.log(myVar);
